d234ee9e9c1b2e2a6d55af7ba1f9477aee71d6ca
AGO523 <unpouletfrais0523@gmail.com>
2024-03-06 12:59:05 +0900

    add: function for create arts

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,5 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
 import hero_tora from "../images/hero_tora.jpg";
-import yagi_neko from "../images/yagi_neko.jpg";
 import inu from "../images/inu.jpg";
 import kame from "../images/kame.jpg";
 import neko from "../images/neko.jpg";
@@ -32,80 +31,33 @@ export default function Index() {
         </div>
       </section>
 
-      <section>
-        <div className="hero min-h-screen bg-base-200">
-          <div className="hero-content flex-col lg:flex-row">
+      <section className="container mx-auto flex flex-row items-center justify-center m-4">
+        <div className="card card-compact w-96 bg-base-100 shadow-xl m-2">
+          <figure>
+            <img src={inu} alt="犬の作品" width={400} height={400} />
+          </figure>
+        </div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl m-2">
+          <figure>
+            <img src={kame} alt="亀の作品" width={400} height={400} />
+          </figure>
+        </div>
+      </section>
+      <section className="container mx-auto flex flex-row items-center justify-center m-4">
+        <div className="card card-compact w-96 bg-base-100 shadow-xl m-2">
+          <figure>
+            <img src={neko} alt="猫の作品" width={400} height={400} />
+          </figure>
+        </div>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl m-2">
+          <figure>
             <img
-              src={yagi_neko}
-              alt="ヤギと猫の作品"
-              className="max-w-sm rounded-lg shadow-2xl"
+              src={mitumeru}
+              alt="犬が見つめる作品"
+              width={400}
+              height={400}
             />
-            <div>
-              <p className="mb-5">
-                動物たちに会うたびに、ただの人間と動物の関係ではなく、一つ一つの存在としての会話を感じます。
-              </p>
-              <p className="mb-5">
-                犬や猫など、特定の種類にとらわれず、この世界で唯一無二の生命として接しています。
-              </p>
-              <p className="mb-5">
-                このようにして、動物や自然の作品を彫り続けたいと思っています。
-                もっと多くの人に、大切な命の存在を知ってほしい。そのための仕事をこれからもやっていきたいです。
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section className="container mx-auto flex flex-row items-center justify-center mt-4">
-        <div className="card card-compact w-96 bg-base-100 shadow-xl">
-          <figure>
-            <img src={inu} alt="犬の作品" />
           </figure>
-          <div className="card-body">
-            <h2 className="card-title">犬の作品</h2>
-            <p>犬の作品です。</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">詳細を見る</button>
-            </div>
-          </div>
-        </div>
-        <div className="card card-compact w-96 bg-base-100 shadow-xl">
-          <figure>
-            <img src={kame} alt="亀の作品" />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">亀の作品</h2>
-            <p>亀の作品です。</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">詳細を見る</button>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="container mx-auto flex flex-row items-center justify-center mt-4">
-        <div className="card card-compact w-96 bg-base-100 shadow-xl">
-          <figure>
-            <img src={neko} alt="猫の作品" />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">猫の作品</h2>
-            <p>猫の作品です。</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">詳細を見る</button>
-            </div>
-          </div>
-        </div>
-        <div className="card card-compact w-96 bg-base-100 shadow-xl">
-          <figure>
-            <img src={mitumeru} alt="犬が見つめる作品" />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">犬が見つめる作品</h2>
-            <p>犬が見つめる作品です。</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">詳細を見る</button>
-            </div>
-          </div>
         </div>
       </section>
     </>
