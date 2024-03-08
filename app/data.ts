e155dcbf10c56a842daa7711d3f2dd8d4a9de9a2
AGO523 <unpouletfrais0523@gmail.com>
2024-03-08 09:38:40 +0900

    fix: admin

--- app/data.ts
+++ app/data.ts
@@ -4,6 +4,7 @@
 import { arts } from "db/schema";
 import { InferInsertModel } from "drizzle-orm";
 import { createClient } from "~/features/common/services/db.server";
+import { json } from "@remix-run/cloudflare";
 
 type CreateArt = InferInsertModel<typeof arts>;
 
@@ -18,6 +19,10 @@ export async function createArt(formData: FormData, env: { DB: D1Database }) {
     createdAt: currentTime,
     updatedAt: currentTime,
   };
-  const response = await db.insert(arts).values(newArt).returning().get();
-  return response;
+  const response = await db.insert(arts).values(newArt).execute();
+  if (response.success) {
+    return json({ message: "作品を投稿しました" }, { status: 201 });
+  } else {
+    return json({ message: "作品の投稿に失敗しました" }, { status: 500 });
+  }
 }
