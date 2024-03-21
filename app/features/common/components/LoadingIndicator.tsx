3c92d390fedb327374dc5875a9c8b36f6667b56c
AGO523 <unpouletfrais0523@gmail.com>
2024-03-22 08:56:55 +0900

    fix: loading

--- app/features/common/components/LoadingIndicator.tsx
+++ app/features/common/components/LoadingIndicator.tsx
@@ -1,8 +1,8 @@
 export default function LoadingIndicator() {
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-      Now loading
-      <span className="loading loading-dots loading-sm text-white"></span>
+      読み込み中
+      <span className="loading loading-dots loading-sm text-base-200"></span>
     </div>
   );
 }
