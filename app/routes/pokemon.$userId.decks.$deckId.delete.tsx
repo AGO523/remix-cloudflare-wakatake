1163852c680c7fa62f6a32d7e565e68f966d7ef7
AGO523 <unpouletfrais0523@gmail.com>
2024-06-01 08:32:13 +0900

    fix: SPA化

--- app/routes/pokemon.$userId.decks.$deckId.delete.tsx
+++ app/routes/pokemon.$userId.decks.$deckId.delete.tsx
@@ -1,20 +1,85 @@
 import type { LoaderFunctionArgs } from "@remix-run/cloudflare";
-import { json } from "@remix-run/cloudflare";
-import { Outlet } from "@remix-run/react";
+import {
+  Form,
+  json,
+  useLoaderData,
+  useNavigation,
+  Link,
+} from "@remix-run/react";
 import { getAuthenticator } from "~/features/common/services/auth.server";
+import {
+  getDeckById,
+  deleteDeck,
+} from "~/features/common/services/deck-data.server";
+import { redirectWithSuccess, redirectWithError } from "remix-toast";
 
-export async function loader({ request, context }: LoaderFunctionArgs) {
+export async function loader({ params, context, request }: LoaderFunctionArgs) {
   const authenticator = getAuthenticator(context);
   const user = await authenticator.isAuthenticated(request, {
     failureRedirect: "/login",
   });
-  return json({ user });
+  const { deckId } = params;
+  if (!deckId) {
+    throw new Response("Deck ID is required", { status: 400 });
+  }
+  const deck = await getDeckById(Number(deckId), context);
+  if (!deck) {
+    throw new Response("Deck not found", { status: 404 });
+  }
+  return json({ deck, user });
 }
 
-export default function DeleteDeckLayout() {
+export const action = async ({ params, context }: LoaderFunctionArgs) => {
+  const { deckId } = params;
+  if (!deckId) {
+    throw new Response("Deck ID is required", { status: 400 });
+  }
+  const response = await deleteDeck(Number(deckId), context);
+  const responseData = await response.json();
+  if (response.status === 200) {
+    return redirectWithSuccess(
+      `/pokemon/${params.userId}/decks`,
+      responseData.message
+    );
+  }
+  return redirectWithError(
+    `/pokemon/${params.userId}/decks`,
+    responseData.message
+  );
+};
+
+export default function DeleteDeck() {
+  const { user } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
   return (
-    <div className="container mx-auto p-2">
-      <Outlet />
+    <div className="flex justify-center items-center">
+      <div className="shadow-md rounded-lg p-8 max-w-3xl w-full">
+        <h2 className="text-2xl font-semibold text-center mb-6">
+          デッキを削除する
+        </h2>
+        <p className="text-error text-center mb-4">
+          この操作は取り消せません。本当に削除しますか？
+          <br />
+          デッキに関連するデータは全て削除されます。
+        </p>
+        <Form method="post" className="space-y-4">
+          <input type="hidden" name="userId" value={user.id} />
+          <div>
+            <button
+              type="submit"
+              className="btn btn-error w-full"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? "デッキを削除しています..." : "削除"}
+            </button>
+          </div>
+        </Form>
+        <Link to="../" className="btn w-full mt-4">
+          キャンセル
+        </Link>
+      </div>
     </div>
   );
 }
